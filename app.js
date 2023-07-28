f55283b63074d14ee09addcc2f9a553907a137a5
Vladislav <69819592+VladislavMac@users.noreply.github.com>
2023-07-28 22:38:42 +0300

    update big fix

--- app.js
+++ app.js
@@ -24,23 +24,56 @@ let key_interval_score;
 let key_interval_line;
 let key_score = 1.00;
 let key_money = 0.0;
-let key_bet = 0;
-let key_button_start = [];
+let key_bet = {};
+let key_width_line = 0;
 
 // #EA0075 #F222A1
 // #F45F35 #FBB462
+
+function setbuttonGet({button : button}){
+    button.id = 'button-get'
+    button.style.background = "linear-gradient(263.87deg, #F45F35 0%, #FBB462 100%)"
+    button.querySelector('#out-money').style.display = 'flex'
+    button.querySelector('#out-title').innerHTML = 'ЗАБРАТЬ'
+
+    button.addEventListener('click', () =>{
+        getBet({button : button})
+    })
+}
+
+function setbuttonCancel({button : button}){
+    button.id = 'button-cancel'
+    button.style.background = "linear-gradient(263.87deg, #EA0075 0%, #F222A1 100%)"
+    button.querySelector('#out-money').style.display = 'none'
+    button.querySelector('#out-title').innerHTML = 'ОТМЕНИТЬ'
+
+    button.addEventListener('click', () =>{
+        returnBet({button : button})
+    })
+}
+
+function setbuttonSet({button : button}){
+    button.id = 'button-bet'
+    button.style.background = "linear-gradient(263.87deg, rgb(148, 78, 245) 0%, rgb(92, 36, 252) 100%)"
+    button.querySelector('#out-money').style.display = 'none'
+    button.querySelector('#out-title').innerHTML = 'СТАВКА'
+
+    button.addEventListener('click', () =>{
+        setBet({button : button})
+    })
+}
 
 function screenEnd(){
     $screen.style.display = 'none';
     $screen.querySelector('#line').style.width = '100%';
 
     key_score = 1.00;
-
-    if( key_bet !== 0 ){
-        key_money = key_money - key_bet;
-        key_button_start.push(button)
-        setMoney()
-        setbuttonGet({button : button});
+    if( Object.keys(key_bet).length !== 0 ){
+        for(const index in key_bet){
+            for(const key in key_bet[index]){
+                setbuttonGet({button : key_bet[index][key]})
+            }
+        }
     }
 
     statusSet();    
@@ -53,7 +86,9 @@ function screenStart(){
     const $line = $screen.querySelector('#line');
     $screen.style.display = 'block';
     key_interval_line = setInterval(() =>{
-        $line.style.width = ($line.getBoundingClientRect().width.toFixed(0) - 1) + 'px';
+        key_width_line = ($line.getBoundingClientRect().width.toFixed(0) - 1);
+
+        $line.style.width = key_width_line + 'px';
         if( $line.style.width === '0px' ){
             clearInterval(key_interval_line);
             screenEnd();    
@@ -62,18 +97,6 @@ function screenStart(){
 }
 
 screenStart()
-
-function setbuttonGet({button : button}){
-    button.style.background = "linear-gradient(263.87deg, #F45F35 0%, #FBB462 100%)"
-    button.querySelector('#out-money').style.display = 'flex'
-    button.querySelector('#out-title').innerHTML = 'ЗАБРАТЬ'
-}
-
-function setbuttonSet({button : button}){
-    button.style.background = "linear-gradient(263.87deg, rgb(148, 78, 245) 0%, rgb(92, 36, 252) 100%)"
-    button.querySelector('#out-money').style.display = 'none'
-    button.querySelector('#out-title').innerHTML = 'СТАВКА'
-}
 
 function setMoney(){
     const $money = document.querySelector('#money');
@@ -108,13 +131,15 @@ function heroFly(){
     $hero.style.top  = y  + 'px';
 }
 
-function scoreStart(){
+function scoreStart(){ // 2 to all button
     key_interval_score = setInterval(() => {
         key_score += 0.01
         $score.innerHTML = `x${key_score.toFixed(2)}`;
-        key_button_start.forEach(button =>{
-            button.querySelector('#out-money').innerHTML = (key_score * key_bet).toFixed(2) + '$'
-        })
+        for(const index in key_bet){
+            for(const key in key_bet[index]){
+                key_bet[index][key].querySelector('#out-money').innerHTML = (key_score * key).toFixed(2) + '$'
+            }
+        }
     }, 100);
 }
 
@@ -129,15 +154,17 @@ function statusSet(status){
 function gameEnd(){
     clearInterval(key_interval_score);
     heroGoodbye();
+    for(const index in key_bet){
+        for(const key in key_bet[index]){
+            setbuttonSet({button : key_bet[index][key]})
+        }
+    }
+    key_bet = {};
+
     setTimeout(()=>{
         heroReturn();
-        key_button_start.forEach(button =>{
-            setbuttonSet({button : button})
-        })
         screenStart()
     }, 3000)
-
-    key_bet = 0;
 }
 
 function lucky(){
@@ -149,15 +176,30 @@ function lucky(){
     }, Math.floor(Math.random() * (time.max - time.min)) + time.min)
 }
 
-function gameStart({button}){
+function returnBet({button}){  
+    // for(const index in key_bet){
+    //     for(const key in key_bet[index]){
+    //         if( index === button.getAttribute('type') ){
+    //             let returmNum = (parseFloat(key)) + key_money;
+    //             key_money = returmNum;
+    //             setMoney();
+    //             setbuttonSet({button: key_bet[index][key]})
+    //             delete key_bet[index]
+    //         }
+    //     }
+    // }
+}
+
+function setBet({button}){
     const $wrapper  = button.parentElement.parentElement;
     const $input    = $wrapper.querySelector('#input-bet');
     const valueBet  = parseFloat($input.value);
-
-    const $hero  = document.querySelector('#hero');
-
-    if( valueBet >= 0.2 && key_money >= 0.2 ){
-        key_bet = valueBet;
+    if( valueBet >= 0.2 && key_money >= 0.2 && key_width_line > 0 ){
+        key_bet[button.getAttribute('type')] = {[valueBet] : button}
+        console.log(key_bet)
+        key_money = key_money - valueBet;
+        setMoney()
+        setbuttonCancel({button : button});
     }
 }
 
@@ -181,9 +223,8 @@ function betPlus({button}){
 setMoney();
 
 $buttonsStart.forEach(button =>{
-    button.addEventListener('click', () =>{
-        gameStart({button : button})
-    })
+        console.log(1)
+    setbuttonSet({button : button})
 })
 
 $buttonsMinus.forEach(button =>{
